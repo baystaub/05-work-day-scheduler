4a9cbf317fdaafa2ddf9bb92af7f03fb12f87bb9
Ducky <staubbm@gmail.com>
2021-03-20 12:39:15 -0600

    updated readme.md, finshed up the buttons for saving, added some images for the readme file. finalizing the code and organizing the variables in js and functions.

--- README.md
+++ README.md
@@ -1,1 +1,101 @@
 # 05-work-day-scheduler
+
+I made this workday schduler for a workday i am used to
+8AM - 5Pm
+
+started off with the html which i made into diverent time blocks with indivudial classes and id's
+
+```
+section class='row' id='1p'>
+            <div class='col-1 justify-content-left teal'>
+                <p>1:00 pm</p>
+            </div>
+            <textarea class='col-10 justify-content-center' id='text1p' placeholder="What to do for the day"></textarea>
+            <button class='col-1 justify-content-right' id='btn1p'>Save</button>
+        </section>
+
+```
+
+gave them all a class of row for the section and than spaced them out with the col-1, col-10 to give them correct spacing for the conetent.
+
+next step was working on the java script
+
+first step for me was getting the time and date up on the header.
+
+![Shows](./images/readme-workdayhw.JPG)
+
+i accomplished this by the following code
+
+```
+var todayDate = moment().format('MMM Do, YYYY')
+var todayTime = moment().format('h:mm:ss a')
+
+$('#currentDay').text(todayDate);
+$("#currentTime").text(todayTime);
+```
+
+which set the time but i kept running into the trouble of the time not currently counting, and only updating on reload. so i had to add a counter kind of like a time looking like
+
+```
+$(document).ready(function() {
+    datetime = $('#currentTime')
+    update();
+    setInterval(update, 1000);
+});
+```
+
+which let the time start counting.
+
+next step was setting the background color for the future, present, and past times
+
+we accomplished this by first setting the if statements based on what time it is.
+
+```
+var now = new Date().getHours();
+
+if (now > 8) {
+        $("#8a").addClass("past");
+    } else if (now >= 8 && now < 9) {
+        $("#8a").addClass("present");
+    } else if (now < 8) {
+        $("#8a").addClass("future");
+    }
+```
+
+we did this for each time slot avaliable.
+
+which basically says that if the hour is past 8 add a past class which made the background a certain color.
+
+if its grater than or equal to 8 and less than 9
+than it will have the class of present.
+else less than 8 will have the class of furture
+depending on the time that is given fron the now variable
+
+once we got on of these completed it was easy to make the rest of them just had to copy and paste the if statmenet 10 times and change the hours in military time counting upwards.
+
+than we went onto the save buttons
+
+```
+function eightA() {
+    var input_textarea = document.querySelector("#text8a");
+    var output_div = document.querySelector("#text8a");
+    var save_button = document.querySelector("#btn8a");
+
+    save_button.addEventListener("click", updateOutput);
+
+    output_div.textContent = localStorage.getItem("content1");
+    input_textarea.value = localStorage.getItem("content1");
+
+    function updateOutput() {
+        localStorage.setItem("content1", input_textarea.value);
+        output_div.textContent = input_textarea.value;
+    }
+}
+```
+
+the issue i went into the save button was delcaring the content we were saving it as in local storage..
+so i had to number then from 1-10 for each hour to have their own storrage
+
+all of this led to the fully functional page that we see here
+
+![fully fuctional page](./images/finalworkday.jpg)
--- assets/logic.js
+++ assets/logic.js
@@ -1,33 +1,201 @@
 //sets current time and date
+//all global variables
 var todayDate = moment().format('MMM Do, YYYY')
 var todayTime = moment().format('h:mm:ss a')
 var datetime = null,
     date = null;
-//inserts those variable into the text at the currenttime and currentdate id's
-$('#currentDay').text(todayDate);
-$("#currentTime").text(todayTime);
-//s
-function setbutton() {
-    text8a.text('');
-    var WTD8 = prompt('What to do?');
-    text8a.text(WTD8);
-    localStorage.setItem('8:00 am', WTD8);
-};
-
-
-
-
-
-
-//DO NOT TOUCH!!!!
-//i will hurt you!!
-
+var now = new Date().getHours();
 var update = function() {
     date = moment(new Date())
     datetime.html(date.format('h:mm a'));
 };
+//inserts those variable into the text at the currenttime and currentdate id's
+$('#currentDay').text(todayDate);
+$("#currentTime").text(todayTime);
 
-var now = new Date().getHours();
+
+//buttons and getting local storage for items
+eightA();
+nineA();
+tenA();
+elevenA();
+twelveA();
+oneP();
+twoP()
+threeP();
+fourP();
+fiveP();
+
+//updates the clock every second.
+$(document).ready(function() {
+    datetime = $('#currentTime')
+    update();
+    setInterval(update, 1000);
+});
+//sets the background for present,past,and future background
+time();
+
+//Buttons  below
+
+function eightA() {
+    var input_textarea = document.querySelector("#text8a");
+    var output_div = document.querySelector("#text8a");
+    var save_button = document.querySelector("#btn8a");
+
+    save_button.addEventListener("click", updateOutput);
+
+    output_div.textContent = localStorage.getItem("content1");
+    input_textarea.value = localStorage.getItem("content1");
+
+    function updateOutput() {
+        localStorage.setItem("content1", input_textarea.value);
+        output_div.textContent = input_textarea.value;
+    }
+}
+
+function nineA() {
+    var input_textarea = document.querySelector("#text9a");
+    var output_div = document.querySelector("#text9a");
+    var save_button = document.querySelector("#btn9a");
+
+    save_button.addEventListener("click", updateOutput);
+
+    output_div.textContent = localStorage.getItem("content2");
+    input_textarea.value = localStorage.getItem("content2");
+
+    function updateOutput() {
+        localStorage.setItem("content2", input_textarea.value);
+        output_div.textContent = input_textarea.value;
+    }
+}
+
+function tenA() {
+    var input_textarea = document.querySelector("#text10a");
+    var output_div = document.querySelector("#text10a");
+    var save_button = document.querySelector("#btn10a");
+
+    save_button.addEventListener("click", updateOutput);
+
+    output_div.textContent = localStorage.getItem("content3");
+    input_textarea.value = localStorage.getItem("content3");
+
+    function updateOutput() {
+        localStorage.setItem("content3", input_textarea.value);
+        output_div.textContent = input_textarea.value;
+    }
+}
+
+function elevenA() {
+    var input_textarea = document.querySelector("#text11a");
+    var output_div = document.querySelector("#text11a");
+    var save_button = document.querySelector("#btn11a");
+
+    save_button.addEventListener("click", updateOutput);
+
+    output_div.textContent = localStorage.getItem("content4");
+    input_textarea.value = localStorage.getItem("content4");
+
+    function updateOutput() {
+        localStorage.setItem("content4", input_textarea.value);
+        output_div.textContent = input_textarea.value;
+    }
+}
+
+function twelveA() {
+    var input_textarea = document.querySelector("#text12a");
+    var output_div = document.querySelector("#text12a");
+    var save_button = document.querySelector("#btn12a");
+
+    save_button.addEventListener("click", updateOutput);
+
+    output_div.textContent = localStorage.getItem("content5");
+    input_textarea.value = localStorage.getItem("content5");
+
+    function updateOutput() {
+        localStorage.setItem("content5", input_textarea.value);
+        output_div.textContent = input_textarea.value;
+    }
+}
+
+function oneP() {
+    var input_textarea = document.querySelector("#text1p");
+    var output_div = document.querySelector("#text1p");
+    var save_button = document.querySelector("#btn1p");
+
+    save_button.addEventListener("click", updateOutput);
+
+    output_div.textContent = localStorage.getItem("content6");
+    input_textarea.value = localStorage.getItem("content6");
+
+    function updateOutput() {
+        localStorage.setItem("content6", input_textarea.value);
+        output_div.textContent = input_textarea.value;
+    }
+}
+
+function twoP() {
+    var input_textarea = document.querySelector("#text2p");
+    var output_div = document.querySelector("#text2p");
+    var save_button = document.querySelector("#btn2p");
+
+    save_button.addEventListener("click", updateOutput);
+
+    output_div.textContent = localStorage.getItem("content7");
+    input_textarea.value = localStorage.getItem("content7");
+
+    function updateOutput() {
+        localStorage.setItem("content7", input_textarea.value);
+        output_div.textContent = input_textarea.value;
+    }
+}
+
+function threeP() {
+    var input_textarea = document.querySelector("#text3p");
+    var output_div = document.querySelector("#text3p");
+    var save_button = document.querySelector("#btn3p");
+
+    save_button.addEventListener("click", updateOutput);
+
+    output_div.textContent = localStorage.getItem("content8");
+    input_textarea.value = localStorage.getItem("content8");
+
+    function updateOutput() {
+        localStorage.setItem("content8", input_textarea.value);
+        output_div.textContent = input_textarea.value;
+    }
+}
+
+function fourP() {
+    var input_textarea = document.querySelector("#text4p");
+    var output_div = document.querySelector("#text4p");
+    var save_button = document.querySelector("#btn4p");
+
+    save_button.addEventListener("click", updateOutput);
+
+    output_div.textContent = localStorage.getItem("content9");
+    input_textarea.value = localStorage.getItem("content9");
+
+    function updateOutput() {
+        localStorage.setItem("content9", input_textarea.value);
+        output_div.textContent = input_textarea.value;
+    }
+}
+
+function fiveP() {
+    var input_textarea = document.querySelector("#text5p");
+    var output_div = document.querySelector("#text5p");
+    var save_button = document.querySelector("#btn5p");
+
+    save_button.addEventListener("click", updateOutput);
+
+    output_div.textContent = localStorage.getItem("content10");
+    input_textarea.value = localStorage.getItem("content10");
+
+    function updateOutput() {
+        localStorage.setItem("content10", input_textarea.value);
+        output_div.textContent = input_textarea.value;
+    }
+}
 
 function time() {
     if (now > 8) {
@@ -101,10 +269,3 @@ function time() {
         $("#5p").addClass("future");
     }
 }
-time();
-//updates the clock every second.
-$(document).ready(function() {
-    datetime = $('#currentTime')
-    update();
-    setInterval(update, 1000);
-});
